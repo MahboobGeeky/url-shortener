9c6efc4f44d8951f4fe8e767a0cf9183ddd29bae
MahboobGeeky <mahboob9184@gmail.com>
2026-09-02 18:20:59 +0530

    added current logged in user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,19 +4,26 @@ import { usersTable } from "../models/user.model.js";
 import { eq } from "drizzle-orm";
 import { createHmac, randomBytes } from "node:crypto";
 import jwt from "jsonwebtoken";
+import {authenticatedUser} from '../middlewares/auth.middleware.js'
 
 const router = express.Router();
 
 // routes
 
+// current logged in user
+router.get("/", authenticatedUser, async (req, res) => {
+  const user = req.user;
+  return res.status(201).json({ user });
+});
+
 // SIGNUP ROUTE
 router.post("/signup", async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
-  const existingUser = await db
+  const [existingUser] = await db
     .select()
     .from(usersTable)
-    .where(usersTable.email, email);
+    .where(eq(usersTable.email, email));
 
   if (existingUser) {
     return res
@@ -26,24 +33,30 @@ router.post("/signup", async (req, res) => {
 
   // generated Hashed Password
   const salt = randomBytes(255).toString("hex");
-  const hashedPassword = createHmac("sha256", salt);
+  const hashedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
 
   // user does not exist, add user into DB
-  const user = await db.insert(usersTable).values({
-    firstname: firstname,
-    lastname: lastname,
-    email: email,
-    password: hashedPassword,
-  });
+  const [user] = await db
+    .insert(usersTable)
+    .values({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: hashedPassword,
+      salt: salt,
+    })
+    .returning();
 
   return res.status(201).json({ status: "success", user });
 });
 
 // LOGIN ROUTE
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  const existingUser = await db
+  const [existingUser] = await db
     .select()
     .from(usersTable)
     .where(eq(email, usersTable.email));
