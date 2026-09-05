1e8bf81f7e10b981c308dc5e7f2343002fb86bbd
MahboobGeeky <mahboob9184@gmail.com>
2026-09-05 23:48:53 +0530

    implemented delete url

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -6,7 +6,7 @@ import { urlsTable } from "../models/url.model.js";
 import { nanoid } from "nanoid";
 import * as z from "zod";
 import { flattenError } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 const router = express.Router();
 
@@ -24,8 +24,6 @@ router.post("/shorten", ensureAuthenticated, async (req, res) => {
   const { url, code } = validationResult.data;
 
   const shortCode = code || nanoid(6);
-
-
 
   const [result] = await db
     .insert(urlsTable)
@@ -47,7 +45,27 @@ router.post("/shorten", ensureAuthenticated, async (req, res) => {
   });
 });
 
-router.get('/:shortcode', async (req, res) => {
+router.delete("/:id", ensureAuthenticated, async (req, res) => {
+  const id = req.params.id;
+
+  await db
+    .delete(urlsTable)
+    .where(and(eq(urlsTable.id, id), eq(urlsTable.userId, req.user.id)));
+
+  return res.status(200).json({ deleted: true });
+});
+
+// Get All Codes Of A User
+router.get("/codes", ensureAuthenticated, async function (req, res) {
+  const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+
+  return res.status(200).json({ codes });
+});
+
+router.get("/:shortcode", async (req, res) => {
   const code = req.params.shortcode;
   const [result] = await db
     .select({
@@ -61,9 +79,6 @@ router.get('/:shortcode', async (req, res) => {
   }
 
   return res.redirect(result.targetURL);
-
-
 });
-
 
 export default router;
